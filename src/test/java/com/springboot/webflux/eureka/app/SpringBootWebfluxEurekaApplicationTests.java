70ebdc3862a9cd9092970055728f72417071e19e
seba365 <seba365@hotmail.com>
2022-10-10 11:39:19 -0300

    First Commit

---
+++ src/test/java/com/springboot/webflux/eureka/app/SpringBootWebfluxEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.webflux.eureka.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
